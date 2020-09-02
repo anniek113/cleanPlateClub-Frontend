8e1ce54fbf0132924bc8cd45c41e395f1f6d64de
anniek113 <anniek113@gmail.com>
2020-09-02 19:12:47 -0400

    Set up navBar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css";
+
+const NavBar = () => {
+    return (
+        <header>
+            <h1>Charleston Clean Plate Club</h1>
+            <p>Contact Us!</p>
+        </header>
+    );
+};
+
+export default NavBar;
